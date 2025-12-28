d393fcb5f6bd296c40ea5c710051cfbe7ab4f39a
Himanshuco <shimanshu0405@gmail.com>
2025-12-28 09:45:04 +0530

    Revert CORS - fix issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,8 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200
 }));
 app.use(express.json());
 connectDB();
